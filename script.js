f8fd243d213230af61266848344d955b10ac4a0e
valoing5 <211050787+valoing5@users.noreply.github.com>
2025-05-18 21:13:34 -0700

    feat: added html and js boilerplate

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('hello')
+
+// pseudocode
+// prompt for user to enter 
+// if invalid choice throw error 
+// select comptuer choice on enter 
+// compare and return result using a bunch of if statements 
