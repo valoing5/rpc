09e89bad66ed2c774ef038a3a26991ef3e2e30b1
valoing5 <211050787+valoing5@users.noreply.github.com>
2025-05-18 21:20:11 -0700

    added functions for player and computer actions
    this is a test commit body, just to see what this looks like on github

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ console.log('hello')
 // if invalid choice throw error 
 // select comptuer choice on enter 
 // compare and return result using a bunch of if statements 
+
+let humanScore = 0;
+let computerScore = 0; 
+
+function getComputerChoice() {
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    if (randNum == 1) {
+        return "rock";
+    } else if (randNum == 2) {
+        return "paper"; 
+    } else {
+        return "scissors"; 
+    }
+}
+
+function getHumanChoice() {
+    const human = prompt('enter r p or s'); 
+    return human; 
+}
+
+console.log(getHumanChoice()); 
